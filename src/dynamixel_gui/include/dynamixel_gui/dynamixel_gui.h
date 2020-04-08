54b7adc48c74362b84f67a62dac6bef15461f31f
Charles Raffaele <u3201136@uni.canberra.edu.au>
2020-04-08 11:11:17 +1000

    it sort of runs

--- src/dynamixel_gui/include/dynamixel_gui/dynamixel_gui.h
+++ src/dynamixel_gui/include/dynamixel_gui/dynamixel_gui.h
@@ -70,65 +70,65 @@ public:
 
   virtual void shutdownPlugin();
 
-  virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
+  //virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
 
-  virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
+  //virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
 
-protected slots:
+//protected slots:
 
-  virtual void updateTopicList();
+  //virtual void updateTopicList();
 
 protected:
 
   // deprecated function for backward compatibility only, use getTopics() instead
-  ROS_DEPRECATED virtual QList<QString> getTopicList(const QSet<QString>& message_types, const QList<QString>& transports);
+  //ROS_DEPRECATED virtual QList<QString> getTopicList(const QSet<QString>& message_types, const QList<QString>& transports);
 
-  virtual QSet<QString> getTopics(const QSet<QString>& message_types, const QSet<QString>& message_sub_types, const QList<QString>& transports);
+  //virtual QSet<QString> getTopics(const QSet<QString>& message_types, const QSet<QString>& message_sub_types, const QList<QString>& transports);
 
-  virtual void selectTopic(const QString& topic);
+  //virtual void selectTopic(const QString& topic);
 
 protected slots:
 
-  virtual void onTopicChanged(int index);
+  //virtual void onTopicChanged(int index);
 
-  virtual void onZoom1(bool checked);
+  //virtual void onZoom1(bool checked);
 
-  virtual void onDynamicRange(bool checked);
+  //virtual void onDynamicRange(bool checked);
 
-  virtual void saveImage();
+  //virtual void saveImage();
 
-  virtual void updateNumGridlines();
+  //virtual void updateNumGridlines();
 
-  virtual void onMousePublish(bool checked);
+  //virtual void onMousePublish(bool checked);
 
-  virtual void onMouseLeft(int x, int y);
+  //virtual void onMouseLeft(int x, int y);
 
-  virtual void onPubTopicChanged();
+  //virtual void onPubTopicChanged();
 
-  virtual void onHideToolbarChanged(bool hide);
+  //virtual void onHideToolbarChanged(bool hide);
 
-  virtual void onRotateLeft();
-  virtual void onRotateRight();
+  //virtual void onRotateLeft();
+  //virtual void onRotateRight();
 
   virtual void testConnect();
 
 protected:
 
-  virtual void callbackImage(const sensor_msgs::Image::ConstPtr& msg);
+  //virtual void callbackImage(const sensor_msgs::Image::ConstPtr& msg);
 
-  virtual void invertPixels(int x, int y);
+  //virtual void invertPixels(int x, int y);
 
-  QList<int> getGridIndices(int size) const;
+  //QList<int> getGridIndices(int size) const;
 
-  virtual void overlayGrid();
+  //virtual void overlayGrid();
 
   Ui::DynamixelGUIWidget ui_;
 
   QWidget* widget_;
 
-  image_transport::Subscriber subscriber_;
+  //image_transport::Subscriber subscriber_;
 
-  cv::Mat conversion_mat_;
+  //cv::Mat conversion_mat_;
 
 private:
 
@@ -158,4 +158,4 @@ private:
 
 }
 
-#endif // rqt_image_view__ImageView_H
+#endif // dynamixel_gui__DynamixelGUI_H
