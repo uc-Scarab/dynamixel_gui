a684a49e5a76e30efee92da2af2cc3eea7b9ad9f
Charles Raffaele <u3201136@uni.canberra.edu.au>
2020-04-26 15:09:23 +1000

    fixed a few errors

--- src/dynamixel_gui/scripts/serial_coms.py
+++ src/dynamixel_gui/scripts/serial_coms.py
@@ -1,9 +1,32 @@
 import rospy
+import serial
+import platform
 from dynamixel_gui.msg import DynamixelPosition
+
+# note that the port on your system may be different, if so make the change here
+if platform.system() == "Windows":
+    arduino = serial.Serial("\\\\.\\COM8")
+elif platform.system() == "Linux":
+    arduino = serial.Serial("/dev/ttyUSB0")
+
+arduino.baudrate = 115200
 
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "stuff %s", data.motor_id)
+
+    dyn_id = (int(data.motor_id)).to_bytes(1, byteorder="little")
+    mode = (int(3)).to_bytes(1, byteorder="little")
+
+    val = (int(data.position)).to_bytes(2, byteorder="little")
+
+    ba_dyn_id = bytearray(dyn_id)
+    ba_mode = bytearray(mode)
+    ba_val = bytearray(val)
+
+    ba_dyn_id.append(ba_mode[0])
+    ba_dyn_id.append(ba_val[0])
+    arduino.write(ba_dyn_id)
 
 
 def listener():
