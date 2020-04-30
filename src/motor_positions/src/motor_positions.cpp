1cb642642b178d8d19fe81c47b13292129790d5f
Charles Raffaele <u3201136@uni.canberra.edu.au>
2020-04-30 12:42:51 +1000

    changed name and created new package

--- src/motor_positions/src/motor_positions.cpp
+++
@@ -1,6 +0,0 @@
-#include "ros/ros.h"
-//#include "dynamixel_gui/DynamixelPosition.h"
-
-int main(){
-    return 0;
-}
---
+++ src/publish_positions.py
@@ -0,0 +1,20 @@
+import serial
+import pdb
+
+arduino = serial.Serial("/dev/ttyUSB1")
+arduino.baudrate = 115200
+
+while True:
+    # motor_2 = arduino.read(2)
+    # motor_3 = arduino.read(2)
+    # motor_4 = arduino.read(2)
+    # motor_5 = arduino.read(2)
+
+    raw = str(arduino.readline())
+
+    motor_positions = list(map(int, raw[2:-5].split(",")))
+    # pdb.set_trace()
+
+    print(motor_positions)
+
+    # pdb.set_trace()
