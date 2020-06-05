223efe0d4d73b22401d555249956ba1baa869ac4
Charles Raffaele <u3201136@uni.canberra.edu.au>
2020-06-05 15:39:05 +1000

    successfully imports ros

--- src/motor_positions/src/publish_positions.cpp
+++ src/motor_positions/src/publish_positions.cpp
@@ -3,25 +3,25 @@
 #include <cstdio>
 #include <unistd.h>
 #include "serial/serial.h"
+#include "ros/ros.h"
+#include <sstream>
 
 
-
-//void my_sleep(unsigned long milliseconds){
-    //#ifdef _WIN32
-        //Sleep(milliseconds);
-    //#else
-        //usleep(milliseconds*1000);
-    //#endif
-//}
+void my_sleep(unsigned long milliseconds){
+    #ifdef _WIN32
+        Sleep(milliseconds);
+    #else
+        usleep(milliseconds*1000);
+    #endif
+}
 
 #define INT_JOIN_BYTE(u, l) (u << 8) | l
 
-int run(){
-    std::string port = "/dev/ttyUSB1";
+int run(int argc, char **argv){
+    ros::init(argc, argv, "publish_positions");
+    std::string port = "/dev/ttyACM0";
     unsigned long baud = 115200;
 
-    //vector<serial::PortInfo> devices_found = serial::list_ports();
-    //std::cout << devices_found;
     serial::Serial arduino(port, baud, serial::Timeout::simpleTimeout(1000));
     if(arduino.isOpen())
         std::cout << "Yes." << std::endl;
@@ -35,16 +35,19 @@ int run(){
         std::cout << int(data_buffer[0]) << std::endl;
 
     for(int i=0;i<10;i+=3){
-        std::cout << "Motor ID:\n" << int(data_buffer[i]) << std::endl; 
-       uint16_t position = INT_JOIN_BYTE(int(data_buffer[i + 2]), int(data_buffer[i + 1])); 
-        std::cout << "Position:\n" << int(position) << std::endl; 
+        //ROS_INFO("test");
+        int id = int(data_buffer[i]);
+        ROS_INFO_STREAM("Motor ID:" << id);
+       uint16_t full_byte = INT_JOIN_BYTE(int(data_buffer[i + 2]), int(data_buffer[i + 1])); 
+       int position = int(full_byte);
+        ROS_INFO_STREAM("Position:" << position); 
     }
     }
     }
 
 
 
-int main(){
-    run();
+int main(int argc, char**argv){
+    run(argc, argv);
     return 0;
 }
