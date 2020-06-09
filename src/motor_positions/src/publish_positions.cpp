e1925f47435e38ddc11fc78b03f14280b65c3aff
Charles Raffaele <u3201136@uni.canberra.edu.au>
2020-06-09 16:51:52 +1000

    not working yet but getting there

--- src/motor_positions/src/publish_positions.cpp
+++ src/motor_positions/src/publish_positions.cpp
@@ -34,15 +34,16 @@ int run(int argc, char **argv){
     std::vector<serial::PortInfo> devices_found = serial::list_ports();
     std::vector<serial::PortInfo>::iterator iter = devices_found.begin();
 
-    while( iter != devices_found.end()){
+    while(iter != devices_found.end()){
         serial::PortInfo device = *iter++;
         //std::cout << "hids" << device.hardware_id << std::endl;
         if (device.hardware_id != "n/a"){
         std::string ids = device.hardware_id.substr(12, 10);
         //std::cout << "hardware:" << device.hardware_id << std::endl;
-        std::cout << "vid_pid:" << vid_pid << std::endl;
-        std::cout << "ids:" << ids << std::endl;
-        //std::cout << "port:" << device.port << std::endl;
+        //std::cout << "description:" << device.description << std::endl;
+        //std::cout << "vid_pid:" << vid_pid << std::endl;
+        //std::cout << "ids:" << ids << std::endl;
+        ////std::cout << "port:" << device.port << std::endl;
         //std::cout << "length" << device.hardware_id.length() << std::endl;
         //std::cout << "substring length" << ids.length() << std::endl;
         if (ids.compare(vid_pid)){
@@ -66,32 +67,41 @@ int run(int argc, char **argv){
     
 
     while(1){
-            uint8_t data_buffer[12];
-        size_t result = arduino.read(data_buffer, 12);
-        std::cout << int(data_buffer[0]) << std::endl;
+        uint8_t check_buffer[3];
+        size_t result = arduino.read(check_buffer, 3);
+        int is_start = int(INT_JOIN_BYTE(check_buffer[1], check_buffer[0]));
+        if(is_start == 65336){
+            int payload = check_buffer[2]; 
+            uint8_t message_buffer[payload];
+            arduino.read(message_buffer, payload);
 
-    motor_positions::MotorPositions msg;
-    int ids[4];
-    int positions[4];
+        motor_positions::MotorPositions msg;
+        int ids[payload/3];
+        int positions[payload/3];
 
-    for(int i=0;i<10;i+=3){
-        //ROS_INFO("test");
-        int id = int(data_buffer[i]);
-        ROS_INFO_STREAM("Motor ID:" << id);
-       uint16_t full_byte = INT_JOIN_BYTE(int(data_buffer[i + 2]), int(data_buffer[i + 1])); 
-       int position = int(full_byte);
-       positions[i] = position;
-        ROS_INFO_STREAM("Position:" << position); 
+        for(int i=0;i<payload-3;i+=3){
+            //ROS_INFO("test");
+            int id = int(message_buffer[i]);
+            ROS_INFO_STREAM("Motor ID:" << id);
+        uint16_t full_byte = INT_JOIN_BYTE(int(message_buffer[i + 2]), int(message_buffer[i + 1])); 
+        int position = int(full_byte);
+        positions[i] = position;
+            ROS_INFO_STREAM("Position:" << position); 
+        }
+
+        if (int(message_buffer[payload - 1] != 244)){
+            arduino.flushInput();
+        }
+
+        for(int i=0; i<payload/3;i++){
+            msg.motor_id = ids[i];
+            msg.position = positions[i];
+            publish.publish(msg);
+        }
+
+        }
     }
-
-    msg.position_2 = positions[0];
-    msg.position_3 = positions[1];    
-    msg.position_4 = positions[2];
-    msg.position_5 = positions[3];
-
-    publish.publish(msg);    
-    }
-    }
+}
 
 
 
