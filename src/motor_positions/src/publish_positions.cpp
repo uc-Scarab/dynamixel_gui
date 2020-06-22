6fc1068083f2ccba698ab0c4ed1bd373b70b37f5
Charles Raffaele <u3201136@uni.canberra.edu.au>
2020-06-22 16:48:49 +1000

    for pi testing

--- src/motor_positions/src/publish_positions.cpp
+++ src/motor_positions/src/publish_positions.cpp
@@ -26,11 +26,14 @@ using std::cout;
 
 class SerialComs {
     public:
-        serial::Serial arduino;
+        serial::Serial write_serial;
+        serial::Serial read_serial;
+
         ros::NodeHandle node;
         SerialComs(ros::NodeHandle out_node) {
         node = out_node;
-    std::string vid_pid = "2341:0043"; 
+    std::string vid_pid = "2341:0043"; // vid/pid for an Arduino Uno
+    std::string diff = "CP2102"; // if the description contains uart its what we want instead of the coms cable.
     std::string port;
 
     std::vector<serial::PortInfo> devices_found = serial::list_ports();
@@ -40,8 +43,9 @@ class SerialComs {
         serial::PortInfo device = *iter++;
         if (device.hardware_id != "n/a"){
         std::string ids = device.hardware_id.substr(12, 10);
-        if (ids.compare(vid_pid)){
+        if (ids.compare(vid_pid) && (device.description.find(diff)!= std::string::npos)){
             port = device.port;
+            std::cout << "description:" << device.description << std::endl;
         }
         }
 
@@ -51,13 +55,19 @@ class SerialComs {
     std::cout << "Port:" << port << std::endl;
     unsigned long baud = 115200;
 
-    serial::Serial arduino(port, baud, serial::Timeout::simpleTimeout(1000));
+    serial::Serial read_serial(port, baud, serial::Timeout::simpleTimeout(1000));
+    serial::Serial write_serial(port, baud, serial::Timeout::simpleTimeout(1000));
     //this->arduino = arduino;
 
-    if(arduino.isOpen())
-        std::cout << "Yes." << std::endl;
+    if(read_serial.isOpen())
+        std::cout << "read open." << std::endl;
     else
-        std::cout << " No." << std::endl;
+        std::cout << "read not open." << std::endl;
+
+    if(write_serial.isOpen())
+        std::cout << "write open." << std::endl;
+    else
+        std::cout << "write not open." << std::endl;
 
 
 
@@ -97,14 +107,15 @@ void subscribe(){
 void run(){
    ros::Publisher publish = this->node.advertise<motor_positions::positionArray>("publish_positions", 1);
    while(1){
+       //std::cout << "running" << std::endl;
        
-        if(this->arduino.available() >= 3){
+        if(this->read_serial.available() >= 3){
             uint8_t check_buffer[3];
-            this->arduino.read(check_buffer, 3);
+            this->read_serial.read(check_buffer, 3);
             uint16_t check = INT_JOIN_BYTE(check_buffer[1], check_buffer[0]);
                 //cout << "check:" << check << std::endl;
                 if(int(check) != 60000){
-                this->arduino.flushInput();
+                this->read_serial.flushInput();
                 cout << "flushed" << std::endl;
                 
                 } else {
@@ -112,7 +123,7 @@ void run(){
                     motor_positions::positionArray msg; 
                     int payload = int(check_buffer[2]);
                     uint8_t message_buffer[payload];
-                    this->arduino.read(message_buffer, payload);
+                    this->read_serial.read(message_buffer, payload);
 
                     for(int i=0;i<payload -3;i+=3){
                         //cout << "id:" << int(message_buffer[i]) << std::endl;
@@ -132,7 +143,7 @@ void run(){
                     }
 
                     if (message_buffer[payload - 1] != 244){
-                    this->arduino.flushInput();
+                    this->read_serial.flushInput();
                     }
         }
                     
@@ -150,10 +161,14 @@ void run(){
 int main(int argc, char**argv){
     ros::init(argc, argv, "publish_positions");
     ros::NodeHandle node;
-    SerialComs serial(node); 
+    SerialComs read_serial(node); 
+    //SerialsComs write_serial(node);
+    
 
-    boost::thread(&SerialComs::run, &serial);
-    boost::thread(&SerialComs::subscribe, &serial);
+    boost::thread read(&SerialComs::run, &read_serial);
+    //boost::thread write(&SerialComs::subscribe, &read_serial);
+    read.join();
+    //write.join();
 
     return 0;
 }
