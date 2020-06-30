6f7faee9860764ff980f9f340f848ffa6d069e81
Charles Raffaele <u3201136@uni.canberra.edu.au>
2020-06-30 14:15:44 +1000

    it works!!!

--- src/motor_positions/src/send_control.cpp
+++ src/motor_positions/src/send_control.cpp
@@ -13,27 +13,27 @@ int main(int argc, char**argv){
    ros::Publisher publish = node.advertise<motor_positions::controlTable>("dynamixel_control", 1);
 
    //bool alt = true;
-    while(1){
+   //
+    while(ros::ok()){
     
 
      
     motor_positions::controlTable msg;
 
-    for(int i = 0; i<6; i++){
-        msg.motor_id = 2;
+    //for(int i=5 ; i<6; i++ ){ 
+        msg.motor_id = 5;
         msg.command_id = 30;
         msg.value = 2048;
-        
+        publish.publish(msg);
+        ROS_INFO_STREAM(msg);
 
 
-    }
+    //}
 
+        usleep(2000000);
         //alt = !alt;
 
-    publish.publish(msg);
-    ROS_INFO_STREAM(msg);
-    usleep(2000000);
-    }
+        }
  
     return 0;
 }
