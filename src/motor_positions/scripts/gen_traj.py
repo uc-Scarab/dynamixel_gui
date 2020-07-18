8274d4bb72b7cd16e94d1831ef84150a4c25a2ce
Charles Raffaele <u3201136@uni.canberra.edu.au>
2020-07-18 16:09:42 +1000

    more interesting kinds of errors

--- src/motor_positions/scripts/gen_traj.py
+++ src/motor_positions/scripts/gen_traj.py
@@ -1,8 +1,38 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 import rospy
-# from std_msgs.msg import String
-# from motor_positions.msg import trajectory
-# from roboticstoolbox import mstraj
-import requests
+from motor_positions.msg import controlTable
+from roboticstoolbox import mstraj
+import numpy as np
+import pdb
+
+pub = rospy.Publisher('dynamixel_control', controlTable, queue_size=10)
+rospy.init_node('trajectories', anonymous=True)
+rate = rospy.Rate(10) # 10hz
+
+path = np.array([
+        [2173, 2526, 1412, 2502],
+        [2105,  1320, 1408, 2703],
+        [2070, 1539, 2394, 2194],
+        [2578, 2394, 2368, 2194]
+        ])
+
+
+out = mstraj(path, dt=0.1, tacc=5, tsegment=[2, 2, 2])
+# pdb.set_trace()
+
+move_msg = controlTable()
+while not rospy.is_shutdown():
+    for move in out.q:
+        print("test")
+        # pdb.set_trace()
+        for count, via in enumerate(move):
+            move_msg.motor_id = count + 1
+            move_msg.command_id = 58
+            move_msg.value = via
+
+            pub.publish(move_msg)
+            rate.sleep()
+
+
 
 
