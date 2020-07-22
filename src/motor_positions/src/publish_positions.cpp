2f348b9070283b40b33008e2922e73dc292db2be
Charles Raffaele <u3201136@uni.canberra.edu.au>
2020-07-22 16:36:28 +1000

    added recieving of positions from teensy

--- src/motor_positions/src/publish_positions.cpp
+++ src/motor_positions/src/publish_positions.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cstdio>
 #include "ros/init.h"
+#include "ros/publisher.h"
 #include "serial/serial.h"
 #include <math.h>
 #include <std_msgs/String.h>
@@ -23,7 +24,6 @@
 using std::vector;
 using std::chrono::system_clock;
 using std::chrono::milliseconds;
-
 
 const std::string port = "/dev/ttyACM0";
 const int baud = 115200;
@@ -72,8 +72,51 @@ void writeSerial(){
 }
 
         
+void readSerial(){
+        if(teensy_serial.available() >= 3){
+            uint8_t check_buffer[3];
+            teensy_serial.read(check_buffer, 3);
+            uint16_t check = INT_JOIN_BYTE(check_buffer[1], check_buffer[0]);
+                if(check != 60000){
+                teensy_serial.flushInput();
+                ROS_ERROR_STREAM("front: check:" << check);
+                
+                } else {
+                    motor_positions::motorPosition motor;
+                    motor_positions::positionArray msg; 
+                    int payload = int(check_buffer[2]);
+                    uint8_t message_buffer[payload];
+                    teensy_serial.read(message_buffer, payload);
 
-        //teensy_serial.write(control_buffer, 8);
+                    for(int i=0;i<payload -3;i+=3){
+                        //cout << "id:" << int(message_buffer[i]) << std::endl;
+                        int int_id = int(message_buffer[i]);
+                        ROS_INFO_STREAM("id:" << int_id);
+                        motor.motor_id = message_buffer[i];
+                        uint16_t full_byte = INT_JOIN_BYTE(message_buffer[i + 2], message_buffer[i + 1]);
+                        //cout << "position" << int(full_byte) << std::endl;
+                        //cout << "---------" << std::endl;
+                        int int_full_byte = int(full_byte);
+                        ROS_INFO_STREAM("position:" << int_full_byte);
+                        motor.position = full_byte;
+                        msg.positions.push_back(motor);
+                    
+                    //publisher.publish(msg);
+
+                    }
+
+                    recievedPositions.push_back(msg);
+
+                    ////if (message_buffer[payload - 1] != 244){
+                        ////ROS_ERROR_STREAM("back check" << message_buffer[payload - 1]);
+                    ////teensy_serial.flushInput();
+                    ////}
+        }
+                    
+                }
+          
+    }
+
 
 
 void controlCallback(motor_positions::controlTable msg) {
@@ -81,77 +124,29 @@ void controlCallback(motor_positions::controlTable msg) {
         controlMessages.push_back(msg);
 }
 
-//void writeControl(){
-    //// provides a function for the write thread can point to since 
-     //ros::Subscriber sub = node.subscribe("dynamixel_control", 1000, &SerialComs::controlCallback, this);
-     //ros::spin();
-//}
+void publishPositions(ros::Publisher pub){
+    if (recievedPositions.size() > 0){
+        for(motor_positions::positionArray array : recievedPositions){
+            pub.publish(array);
+        }
 
-//void receivePositions(int baud, std::string port){
-    //// recieves position updates from the teensy
-   //ros::Publisher publisher = node.advertise<motor_positions::positionArray>("publisher_positions", 1);
-       //serial::Serial teensy_serial(port, baud, serial::Timeout::simpleTimeout(1000));
-
-   //while(1){
-        //if(teensy_serial.available() >= 3){
-            //uint8_t check_buffer[3];
-            //teensy_serial.read(check_buffer, 3);
-            //uint16_t check = INT_JOIN_BYTE(check_buffer[1], check_buffer[0]);
-                //if(check != 60000){
-                //teensy_serial.flushInput();
-                //ROS_ERROR_STREAM("front: check:" << check);
-                ////cout << "flushed" << std::endl;
-                
-                //} else {
-                    //motor_positions::motorPosition motor;
-                    //motor_positions::positionArray msg; 
-                    //int payload = int(check_buffer[2]);
-                    //uint8_t message_buffer[payload];
-                    //teensy_serial.read(message_buffer, payload);
-
-                    //for(int i=0;i<payload -3;i+=3){
-                        ////cout << "id:" << int(message_buffer[i]) << std::endl;
-                        //int int_id = int(message_buffer[i]);
-                        //ROS_INFO_STREAM("id:" << int_id);
-                        //motor.motor_id = message_buffer[i];
-                        //uint16_t full_byte = INT_JOIN_BYTE(message_buffer[i + 2], message_buffer[i + 1]);
-                        ////cout << "position" << int(full_byte) << std::endl;
-                        ////cout << "---------" << std::endl;
-                        //int int_full_byte = int(full_byte);
-                        //ROS_INFO_STREAM("position:" << int_full_byte);
-                        //motor.position = full_byte;
-                        //msg.positions.push_back(motor);
-                    
-                    //publisher.publish(msg);
-
-                    //}
-
-                    ////if (message_buffer[payload - 1] != 244){
-                        ////ROS_ERROR_STREAM("back check" << message_buffer[payload - 1]);
-                    ////teensy_serial.flushInput();
-                    ////}
-        //}
-                    
-                //}
-          
-    //}
-//}
-
-
-
-        //};
+    }
+}
 
 
 int main(int argc, char**argv){
+    ros::init(argc, argv, "publish_positions");
+    ros::NodeHandle node;
+
+    ros::Subscriber sub = node.subscribe("dynamixel_control", 1000, controlCallback);
+
+   ros::Publisher publisher = node.advertise<motor_positions::positionArray>("publisher_positions", 1);
+
     auto last_activation = std::chrono::high_resolution_clock::now();
 ;
     auto time_now = std::chrono::high_resolution_clock::now();
 ;
-    ros::init(argc, argv, "publish_positions");
-    ros::NodeHandle node;
-    ros::Subscriber sub = node.subscribe("dynamixel_control", 1000, controlCallback);
-
-    while(ros::ok()){
+        while(ros::ok()){
 
     time_now = std::chrono::high_resolution_clock::now();
     auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(time_now - last_activation).count(); 
@@ -161,8 +156,22 @@ int main(int argc, char**argv){
         last_activation = time_now;
     }
 
+    if(diff >= 100){
+        readSerial();
+        last_activation = time_now;
+    }
+
+
+
+    if(diff >= 100){
+        publishPositions(publisher);
+        last_activation = time_now;
+    }
+
+
     ros::spinOnce();
     rate.sleep();
+
     }
  
     //SerialComs teensy_serial(node); 
