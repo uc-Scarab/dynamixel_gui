a314159776ef7185ba98f324b29d3d88fe617838
Charles Raffaele <u3201136@uni.canberra.edu.au>
2020-08-05 15:41:32 +1000

    basic trajectories without overshoot

--- src/motor_positions/src/gen_traj.py
+++ src/motor_positions/src/gen_traj.py
@@ -8,35 +8,36 @@ import pdb
 
 # from std_msgs.msg import String
 
+def radstoRaw(rads):
+    raw = rads/ 0.00153589  
+    raw += 2048
+    return(raw)
+
 rospy.init_node('trajectories', anonymous=True)
-pub = rospy.Publisher("dynamixel_control", controlTable, queue_size=10)
+pub = rospy.Publisher("dynamixel_control", controlTable, queue_size=60)
 rate = rospy.Rate(10) # 10hz
 msg = controlTable()
 
 path = np.array([
-        [2173, 2526, 1412, 2502],
-        [2105,  1320, 1408, 2703],
-        [2070, 1539, 2394, 2194],
-        [2578, 2394, 2368, 2194]
+        [0, 0, 0, 0],
+        [0, 0.4401, -0.4098, -0.0569]
         ])
 
 
-out = mstraj(path, dt=0.1, tacc=1, tsegment=[2, 2, 2])
-
+out = mstraj(path, dt=0.1, tacc=1, tsegment=[2])
 move_msg = controlTable()
 
-while True:
-    for move in out.q:
-        # pdb.set_trace()
-        for count, via in enumerate(move):
-            move_msg.motor_id = count + 4
-            move_msg.command_id = 58
-            move_msg.value = math.ceil(via)
-            # print(move_msg.value)
+for move in out.q:
+    # pdb.set_trace()
+    for count, via in enumerate(move):
+        move_msg.motor_id = count + 1 
+        move_msg.command_id = 58
+        move_msg.value = math.ceil(radstoRaw(via))
+        # print(move_msg.value)
 
-            rospy.loginfo(move_msg)
-            pub.publish(move_msg)
-        rate.sleep()
+        rospy.loginfo(move_msg)
+        pub.publish(move_msg)
+    rate.sleep()
 
 
 
