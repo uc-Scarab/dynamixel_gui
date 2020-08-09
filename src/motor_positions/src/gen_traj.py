6858dc426c3fdc0f055a2985b7f3e13b36a17e10
Charles Raffaele <u3201136@uni.canberra.edu.au>
2020-08-09 14:59:58 +1000

    will now move, still to wrong place

--- src/motor_positions/src/gen_traj.py
+++ src/motor_positions/src/gen_traj.py
@@ -3,7 +3,7 @@ import rospy
 from motor_positions.msg import controlTable
 from roboticstoolbox import mstraj
 import numpy as np
-import math
+from math import pi, ceil
 import pdb
 from time import sleep
 from inv_kine import leg_ikine
@@ -19,19 +19,24 @@ rospy.init_node('trajectories', anonymous=True)
 pub = rospy.Publisher("dynamixel_control", controlTable, queue_size=60)
 rate = rospy.Rate(10) # 10hz
 msg = controlTable()
+a = [0.62, 0.86, 0.70, 0.61]
 
+test = np.array(leg_ikine(1.5, 0, 0, -pi/2, -1, a))
 
-
-out = mstraj(path, dt=0.2, tacc=0.5, tsegment=[1])
-# pdb.set_trace()
+zeros = np.array(
+        [0, 0, 0, 0],
+        )
+path = np.row_stack((zeros, test))
+# out = mstraj(path, dt=0.2, tacc=0.5, tsegment=[1])
+pdb.set_trace()
 move_msg = controlTable()
 
-for move in out.q:
+for move in path:
     # pdb.set_trace()
     for count, via in enumerate(move):
         move_msg.motor_id = count + 1
         move_msg.command_id = 58
-        move_msg.value = math.ceil(radstoRaw(via))
+        move_msg.value = ceil(radstoRaw(via))
         # print(move_msg.value)
 
         rospy.loginfo(move_msg)
--- src/motor_positions/src/inv_kine.py
+++ src/motor_positions/src/inv_kine.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 import rospy
-import numpy
+import pdb
 from math import atan2, sqrt, sin, cos, acos, pi
 
 def leg_ikine(x_in, y_in, z_in, o_in, f, a):
@@ -8,7 +8,7 @@ def leg_ikine(x_in, y_in, z_in, o_in, f, a):
         q = [0, 0, 0, 0]
         q[0] = atan2(y_in, x_in)
         r1 = sqrt(x_in**2 + y_in**2) - a[0]
-        r2 = sqrt(float(z_in**2) + r1**2)
+        r2 = sqrt((z_in**2) + r1**2)
         p1 = atan2(z_in, r1)
         r3 = a[3] * sin(-1 * o_in)
         r4 = a[3] * cos(o_in)
@@ -17,28 +17,30 @@ def leg_ikine(x_in, y_in, z_in, o_in, f, a):
         r7 = sqrt(r6**2 + r5**2)
         p2 = atan2(r5, r6)
 
-        p3 = f * acos( a[1]**2 + r7**2 - a[2]**2) / (2 * a[1] * r7)
-        p4 = f * acos( a[2]**2 + a[1]**2 - r7**2) / (2 * a[2] * a[1])
+        p3 = f * acos( (a[1]**2 + r7**2 - a[2]**2) / (2 * a[1] * r7))
+        p4 = f * acos( (a[2]**2 + a[1]**2 - r7**2) / (2 * a[2] * a[1]))
 
         q[2] = pi - p4
         if f == -1:
-            q[2] = q[2] - pi
+            q[2] = q[2] - 2 * pi
 
         q[1] = p2 - p3
 
         q[3] = o_in - (q[1] + q[2])
 
-        numpy_q = numpy.array(q)
+        q[3] = 3.1455 - q[3]
 
-        return(numpy_q)
+        return(q)
 
     except ValueError:
         # print("Test")
         rospy.loginfo("Got a ValueError, probably a maths problem")
 
 
-# leg_ikine(3, 3, 3, 3, 3, [3, 3, 3, 3])
-
+if __name__ == "__main__":
+    a = [0.62, 0.86, 0.70, 0.61]
+    test = leg_ikine(1.5, 0, 0, -pi/2, -1, a)
+    pdb.set_trace()
 
 
 
