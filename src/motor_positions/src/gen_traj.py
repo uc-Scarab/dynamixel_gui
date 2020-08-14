125116e29811445fd6b3fd2594d3215eb1b146f5
Charles Raffaele <u3201136@uni.canberra.edu.au>
2020-08-14 13:00:05 +1000

    $#!% ROS! serial_coms isn't properly recieving all the messages to turn the torque/speed on and off.

--- src/motor_positions/src/gen_traj.py
+++ src/motor_positions/src/gen_traj.py
@@ -3,70 +3,51 @@ import rospy
 from motor_positions.msg import controlTable
 from roboticstoolbox import mstraj
 import numpy as np
-from math import pi, ceil
+import math
 import pdb
 from time import sleep
-from inv_kine import leg_ikine
 
 # from std_msgs.msg import String
 
 def radstoRaw(rads):
     raw = rads/ 0.00153589
     raw += 2048
-    return(int(raw))
+    return(raw)
 
 rospy.init_node('trajectories', anonymous=True)
-pub = rospy.Publisher("dynamixel_control", controlTable, queue_size=60)
+pub = rospy.Publisher("dynamixel_control", controlTable, queue_size=100)
 rate = rospy.Rate(10) # 10hz
 msg = controlTable()
-a = [0.62, 0.86, 0.70, 0.61]
-
-test = np.array(leg_ikine(1.5, 0, 0, -pi/2, -1, a))
-
-# zeros = np.array(
-        # [0, 0, 0, 0],
-        # )
-
-# qz = [0, 0, 0, 0]
-# qn = [0, 0.9484, -1.9477, -0.5715]
-setup_msg = controlTable()
 
 path = np.array([
-        [0, 0.4401, 0.6496, 0.7019],
+        [0, -0.4401, -0.6496, -0.7019],
         [0, 0, 0, 0],
-        [0, -0.4401, -0.6496, -0.7019]
+        [0, 0.4401, 0.6496, 0.7019]
         ])
 
 
-
-# path = np.row_stack((qz, qn))
-out = mstraj(path, dt=0.1, tacc=1, tsegment=[2, 2])
+out = mstraj(path, dt=0.2, tacc=0.5, tsegment=[1, 1])
+# pdb.set_trace()
 for i in range(10, 14):
-    setup_msg.dest = i
-    setup_msg.command_id = 2
-    setup_msg.value = 500
-    rospy.loginfo(setup_msg)
-    pub.publish(setup_msg)
-
-    setup_msg.command_id = 5
-    rospy.loginfo(setup_msg)
-    pub.publish(setup_msg)
-    sleep(0.1)
-
-print("finish setup")
+    move_msg = controlTable()
+    move_msg.dest = i
+    move_msg.command_id = 2
+    move_msg.value = 500
+    rospy.loginfo(move_msg)
+    pub.publish(move_msg)
+    sleep(2)
 
 pdb.set_trace()
-move_msg = controlTable()
-
 for move in out.q:
+    # pdb.set_trace()
     for count, via in enumerate(move):
         move_msg.dest = count + 10
-        move_msg.command_id = 0
-        move_msg.value = ceil(radstoRaw(via))
+        move_msg.command_id = 2
+        move_msg.value = 0
+        # print(move_msg.value)
 
         rospy.loginfo(move_msg)
         pub.publish(move_msg)
-        sleep(0.1)
-
+    sleep(0.1)
 
 
--- src/motor_positions/src/test.py
+++ src/motor_positions/src/test.py
@@ -1,23 +1,10 @@
-#! /usr/bin/env python3
 import rospy
 from motor_positions.msg import controlTable
-from roboticstoolbox import mstraj
-import numpy as np
-from math import pi, ceil
-import pdb
-from time import sleep
-from inv_kine import leg_ikine
 
 # from std_msgs.msg import String
 
-def radstoRaw(rads):
-    raw = rads/ 0.00153589
-    raw += 2048
-    return(int(raw))
-
-rospy.init_node('trajectories', anonymous=True)
-pub = rospy.Publisher("dynamixel_control", controlTable, queue_size=60)
-rate = rospy.Rate(10) # 10hz
+rospy.init_node('test')
+pub = rospy.Publisher('dynamixel_control', controlTable, queue_size=100)
 
 pose1 = [0, 0.4401, 0.6496, 0.7019]
 pose2 = [0, -0.4401, -0.6496, -0.7019]
@@ -25,35 +12,40 @@ pose2 = [0, -0.4401, -0.6496, -0.7019]
 setup_msg = controlTable()
 # path = np.row_stack((qz, qn))
 # out = mstraj(path, dt=0.1, tacc=1, tsegment=[2, 2])
-for i in range(10, 14):
-    setup_msg.dest = i
-    setup_msg.command_id = 2
-    setup_msg.value = 500
-    rospy.loginfo(setup_msg)
-    pub.publish(setup_msg)
+setup_msg.dest = 10
+setup_msg.command_id = 2
+setup_msg.value = 0
+pub.publish(setup_msg)
 
-    setup_msg.command_id = 5
-    rospy.loginfo(setup_msg)
-    pub.publish(setup_msg)
-    sleep(0.1)
+# for i in range(10, 14):
+    # setup_msg.dest = i
+    # setup_msg.command_id = 2
+    # setup_msg.value = 500
+    # rospy.loginfo(setup_msg)
+    # pub.publish(setup_msg)
+
+    # setup_msg.command_id = 5
+    # rospy.loginfo(setup_msg)
+    # pub.publish(setup_msg)
+    # sleep(0.1)
 
 print("finish setup")
 
-pdb.set_trace()
-move_msg = controlTable()
-while not rospy.is_shutdown():
-    test = input()
-    if test == "1":
-        move = pose1
-    elif test == "2":
-        move = pose2
+# pdb.set_trace()
+# move_msg = controlTable()
+# while not rospy.is_shutdown():
+    # test = input()
+    # if test == "1":
+        # move = pose1
+    # elif test == "2":
+        # move = pose2
 
-    for count, via in enumerate(move):
-        move_msg.dest = count + 10
-        move_msg.command_id = 0
-        move_msg.value = ceil(radstoRaw(via))
+    # for count, via in enumerate(move):
+        # move_msg.dest = count + 10
+        # move_msg.command_id = 0
+        # move_msg.value = ceil(radstoRaw(via))
 
-        rospy.loginfo(move_msg)
-        pub.publish(move_msg)
-        sleep(0.1)
+        # rospy.loginfo(move_msg)
+        # pub.publish(move_msg)
+        # sleep(0.1)
 
