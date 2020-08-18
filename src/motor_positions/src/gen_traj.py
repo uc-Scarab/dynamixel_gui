e5444ebf40afb0d8a301f75773d702a00d28ff86
Charles Raffaele <u3201136@uni.canberra.edu.au>
2020-08-18 15:58:30 +1000

    added a few things for testing

--- src/motor_positions/src/gen_traj.py
+++ src/motor_positions/src/gen_traj.py
@@ -25,15 +25,33 @@ path = np.array([
         [0, 0.4401, 0.6496, 0.7019]
         ])
 
+path2 = np.array([
+        [0, 0.4401, 0.6496, 0.7019],
+        [0, 0, 0, 0],
+        [0, -0.4401, -0.6496, -0.7019]
+        ])
 
-out = mstraj(path, dt=0.2, tacc=0.5, tsegment=[1, 1])
-# pdb.set_trace()
+
+
+def movetoTrajectories(path):
+    move_msg = controlTable()
+
+    out = mstraj(path, dt=0.2, tacc=0.5, tsegment=[1, 1])
+    for move in out.q:
+        for count, via in enumerate(move):
+            move_msg.dest = count + 10
+            move_msg.command_id = 0
+            move_msg.value = ceil(radstoRaw(via))
+
+            rospy.loginfo(move_msg)
+            pub.publish(move_msg)
+
 sleep(5)
 for i in range(10, 14):
     move_msg = controlTable()
     move_msg.dest = i
     move_msg.command_id = 5
-    move_msg.value = 100
+    move_msg.value = 200
 
     pub.publish(move_msg)
     rospy.loginfo(move_msg)
@@ -47,13 +65,10 @@ for i in range(10, 14):
     rospy.loginfo(move_msg)
     pub.publish(move_msg)
 
-pdb.set_trace()
-for move in out.q:
-    # pdb.set_trace()
-    for count, via in enumerate(move):
-        move_msg.dest = count + 10
-        move_msg.command_id = 0
-        move_msg.value = 2048
 
-        rospy.loginfo(move_msg)
-        pub.publish(move_msg)
+while not rospy.is_shutdown():
+    move_to = int(input())
+    if move_to == 1:
+        movetoTrajectories(path)
+    elif move_to == 2:
+        movetoTrajectories(path2)
